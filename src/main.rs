633cad8649288188e91d04d86b77dafab124de8f
Jashanpreet singh <sjass6789@gmail.com>
2026-02-28 13:11:38 +0530

    Part #1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
+use std::io::{Read, prelude::*};
 use std::net::{TcpListener, TcpStream};
-use std::io::{prelude::*, Read};
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
@@ -12,8 +12,19 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    let contents = fs::read_to_string("index.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, file) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(file).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
